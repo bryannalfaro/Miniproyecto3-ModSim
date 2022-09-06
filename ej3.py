743ce7958fe7d57e46c5a7825a37bd999a8ff80b
Bryann Alfaro <alf19372@uvg.edu.gt>
2022-09-06 11:54:09 -0600

    comentando archivos

--- ej3.py
+++ ej3.py
@@ -1,8 +1,15 @@
+#Universidad del Valle de Guatemala
+#Modelacion y simulacion
+#Ejercicio 3 - Miniproyecto 3
+#Autores:
+#   - 19372 Bryann Alfaro
+#   - 19422 Diego Arredondo
 import numpy as np
 import numpy_financial as npf
 from matplotlib import pyplot as plt
 
-
+#Funcion donde se encuentran los resultados de las distribuciones (normal, uniforme)
+#para el hotel
 def sim_hotel():
     lista_npv_hotel = [-800]
     lista_npv_hotel.append(np.random.normal(-800,50))
@@ -14,6 +21,8 @@ def sim_hotel():
     lista_npv_hotel.append( np.random.uniform(200,8440))
     return lista_npv_hotel
 
+#Funcion donde se encuentran los resultados de las distribuciones (normal, uniforme)
+#para el centro comercial
 def sim_centro():
     lista_npv_centro = [-900]
     lista_npv_centro.append(np.random.normal(-600,50))
@@ -25,31 +34,31 @@ def sim_centro():
     lista_npv_centro.append(np.random.uniform(1600,6000))
     return lista_npv_centro
 
+#Inicializacion de variables
 valorSimulacion = 1000
 resultado_hotel = 0
 resultado_centro = 0
 
 
-lista1 = []
-lista2 = []
-
-
+#Se realiza la simulacion por la cantidad indicada
 for i in range(valorSimulacion):
-    #Hotel
+    #Se llama a la funcion que simula el hotel
     lista_hotel = sim_hotel()
-    #Centro comercial
+    #Se llama a la funcion que simula el centro comercial
     lista_centro = sim_centro()
+
+    #Se calcula el npv para el hotel y centro comercial al 10%
     r1 = npf.npv(0.1, lista_hotel)
     r2 = npf.npv(0.1, lista_centro)
 
-    lista1.append(r1)
-    lista2.append(r2)
-
+    #Se almacena el resultado del npv
     resultado_hotel += r1
     resultado_centro += r2
 
+#Se calcula el promedio de los resultados
 promedio1 = resultado_hotel/valorSimulacion
 promedio2 = resultado_centro/valorSimulacion
 
+#Se imprime el resultado
 print(f'Promedio del hotel con {valorSimulacion} ejecuciones: ',promedio1)
 print(f'Promedio del centro comercial con {valorSimulacion} ejecuciones: ',promedio2)
--- ej4.py
+++ ej4.py
@@ -1,8 +1,15 @@
+#Universidad del Valle de Guatemala
+#Modelacion y simulacion
+#Ejercicio 4 - Miniproyecto 3
+#Autores:
+#   - 19372 Bryann Alfaro
+#   - 19422 Diego Arredondo
 import random
+import re
 
 
+#Settear valores de costos
 probabilities = [0.3, 0.4, 0.3]
-
 price_papers = 1.5
 sale_price = 2.5
 refund = 0.5
@@ -12,47 +19,85 @@ refund = 0.5
 #Simulacion :
 iterations = [30,360, (360*10)]
 
+#Se recorre el array de iterations
 for i in range(len(iterations)):
-    #revenue al vender
+    #Se inicializan las variables para almacenar ganancias
     revenue9 = 0
     revenue10 = 0
     revenue11 = 0
-    for j in range(iterations[i]):
-        u= random.random()
-        #(Vendi - gaste) + reembolso
-        #sale price * pidieron - price_papers * compre
-        if u <= probabilities[0]:
-            #me pidieron 9
 
-            #Compre 9
+    #Se inicializan las variables para las perdidas
+    loss9 = 0
+    loss10 = 0
+    loss11 = 0
+    #Se recorre la cantidad de veces (30,360,3600)
+    for j in range(iterations[i]):
+        u= random.random() # Se genera valor aleatorio
+
+
+        #Formula a utilizar para calculo de ganancia
+        #(Vendi - gaste) + reembolso
+
+        #Se utiliza transformacion inversa para la evaluacion de la probabilidad
+        if u <= probabilities[0]:
+            #Se evalua si me pidieron 9 periodicos
+
+            #Compre 9 periodicos
             revenue9 += (sale_price*9 - price_papers*9)
-            #Compre 10
+            #Compre 10 periodicos
             revenue10 += (sale_price*9 - price_papers*10)+refund
-            #Compre 11
+            #Compre 11 periodicos
             revenue11 += (sale_price*9 - price_papers*11)+refund*2
 
         elif u <= (probabilities[1]+probabilities[0]) and u>probabilities[0]:
-            #me pidieron 10
+            #Se evalua si me pidieron 10 periodicos
 
-            #Compre 9
-            revenue9 += (sale_price*9 - price_papers*9) #PREGUNTAR, SI ME HACE FALTA, y si esta bien lo de cambiar los sale price porque no pude vendder esos si no los tengo
-            #Compre 10
+            #Compre 9 periodicos
+            revenue9 += (sale_price*9 - price_papers*9)
+            loss9 +=1
+            #Compre 10 periodicos
             revenue10 += (sale_price*10 - price_papers*10)
-            #Compre 11
+            #Compre 11 periodicos
             revenue11 += (sale_price*10 - price_papers*11)+refund
         elif u <= (probabilities[2]+probabilities[1]+probabilities[0]) and u>(probabilities[1]+probabilities[0]):
-            #me pidieron 11
+            #Se evalua si me pidieron 11 periodicos
 
-            #Compre 9
+            #Compre 9 periodicos
             revenue9 += (sale_price*9 - price_papers*9)
-            #Compre 10
+            loss9 +=2
+            #Compre 10 periodicos
             revenue10 += (sale_price*10 - price_papers*10)
-            #Compre 11
+            loss10 +=1
+            #Compre 11 periodicos
             revenue11 += (sale_price*11 - price_papers*11)
 
+    #Se imprime el resultado de la simulacion
+    print("*"*50)
     print(f"Ganancia total simulando para {iterations[i]} dias\n")
-    print(revenue9)
-    print(revenue10)
-    print(revenue11)
+    print(revenue9 ,'para 9 periodicos')
+    print(revenue10,'para 10 periodicos')
+    print(revenue11,'para 11 periodicos')
+
+    print(f"Perdida total simulando para {iterations[i]} dias\n")
+    print(loss9,'para 9 periodicos')
+    print(loss10,'para 10 periodicos')
+    print(loss11,'para 11 periodicos')
+    print("*"*50)
+
+    #Se evalua la mejor cantidad de periodicos a comprar
+    revenues ={9:revenue9, 10:revenue10, 11:revenue11}
+    #Se ordena de mayor a menor el diccionario
+    revenues = sorted(revenues.items(), key=lambda x: x[1], reverse=True)
+    losses = {'9':loss9, '10':loss10, '11':loss11}
+
+    #Se obtienen los 2 primeros valores mayores y se les resta su perdida
+    first = revenues[0][1] - losses.get(str(revenues[0][0]))
+    second = revenues[1][1] - losses.get(str(revenues[1][0]))
+
+    #Se evalua si el primer elemento sigue siendo el de mayor ganancia o no
+    if first>second:
+        print(f"Comprar {revenues[0][0]}")
+    else:
+        print(f"Comprar {revenues[1][0]}")
 
 
